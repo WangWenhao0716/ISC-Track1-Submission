8635b708e41f08aefb663072d79895a72a9754d7
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-09 20:56:45 +0800

    Add files via upload

---
+++ Training/script/Train_color_CC_50.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 python train_single_source_gem_coslr_wb_high_balance.py \
+-ds isc_100k_256_big_color_p4 -a resnet50 --margin 0.0 \
+--num-instances 4 -b 128 -j 8 --warmup-step 5 \
+--lr 0.00035 --iters 8000 --epochs 25 \
+--data-dir /dev/shm/ \
+--logs-dir logs/color_CC/50 \
+--height 256 --width 256
---
+++ Training/script/Train_face_CC_ibn.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_single_source_gem_coslr_wb_high_balance.py \
+-ds isc_100k_256_big_ff -a resnet_ibn50a --margin 0.0 \
+--num-instances 4 -b 128 -j 8 --warmup-step 5 \
+--lr 0.00035 --iters 8000 --epochs 25 \
+--data-dir /dev/shm/ \
+--logs-dir logs/face_CC/ibn \
+--height 256 --width 256
