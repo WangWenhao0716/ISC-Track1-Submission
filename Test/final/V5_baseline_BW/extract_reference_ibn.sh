fb0a0d550081f5aeb78f2c87876767b03e957a3b
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-12 01:09:17 +0800

    Add files via upload

---
+++ Test/final/V5_baseline_BW/extract_reference_ibn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=5 python extract_features.py \
+         --file_list ./list_files/train \
+         --image_dir /dev/shm/training_images \
+         --pca_file ./ibn/pca_byol.vt \
+         --n_train_pca 20000 \
+         --train_pca \
+         --model ibn  --GeM_p 3 --checkpoint baseline_bw_ibn.pth.tar  --imsize 256 --bw \
+
+CUDA_VISIBLE_DEVICES=5 python extract_features.py \
+      --file_list ./list_files/references \
+      --image_dir /dev/shm/reference_images \
+      --o ./ibn/references_byol.hdf5 \
+      --pca_file ./ibn/pca_byol.vt \
+      --model ibn  --GeM_p 3 --checkpoint baseline_bw_ibn.pth.tar --imsize 256 --bw \
---
+++ Test/final/V5_baseline_BW/extract_training_152.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=7 python extract_features.py \
+  --file_list ./list_files/train \
+  --image_dir /dev/shm/training_images \
+  --o ./152/train_byol.hdf5 \
+  --batch_size 500 --pca_file ./152/pca_byol.vt \
+  --model 152  --GeM_p 3 --checkpoint baseline_bw_152.pth.tar --imsize 256 --bw
