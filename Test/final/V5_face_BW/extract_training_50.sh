f17661ef0b5564a5956139a25b159e66e4c1ba87
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-12 01:24:07 +0800

    Add files via upload

---
+++ Test/final/V5_face_BW/extract_training_50.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=1 python extract_features.py \
+  --file_list ./list_files/train \
+  --image_dir /dev/shm/training_images \
+  --o ./50/train_byol.hdf5 \
+  --batch_size 500 --pca_file ./50/pca_byol.vt \
+  --model 50  --GeM_p 3 --checkpoint face_bw_50.pth.tar --imsize 256 --bw
