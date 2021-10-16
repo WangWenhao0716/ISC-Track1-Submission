b93deb96827a3e3e087bf398d20e1c4e8ab7ca04
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-16 17:21:19 +0800

    Update generate_file_list_r.py

--- Test/augmentation/reference/generate_file_list_r.py
+++ Test/augmentation/reference/generate_file_list_r.py
@@ -1,11 +1,16 @@
 import os
-names = sorted(os.listdir('/dev/shm/reference_images_exp/'))
-names = [i[:-4] for i in names]
+names = sorted(os.listdir('/dev/shm/'))
+names = ['/dev/shm/' + name for name in names if 'reference_images_exp_' in name]
+all_names = []
+for i in range(len(names)):
+    ls = sorted(os.listdir(names[i]))
+    for j in range(len(ls)):
+        all_names.append(names[i] + '/' + ls[j])
 
 f = open('dev_reference_exp','a')
-for i in range(len(names)):
+for i in range(len(all_names)):
     if(i==0):
-        f.write(names[i]) 
+        f.write(all_names[i]) 
     else:
-        f.write('\n' + names[i]) 
+        f.write('\n' + all_names[i]) 
 f.close()
