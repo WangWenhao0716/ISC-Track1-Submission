78c15dae0118c5833de4983ea70ec0dbbacfbf29
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-18 22:15:10 +0800

    Create Train_baseline_CC_50_pair.sh

---
+++ Training/script/Train_baseline_CC_50_pair.sh
@@ -0,0 +1,7 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train_single_source_gem_coslr_wb_high_balance_pair.py \
+-ds pairs -a resnet50 --margin 0.0 \
+--num-instances 4 -b 128 -j 8 --warmup-step 5 \
+--lr 0.00035 --iters 800 --epochs 25 \
+--data-dir /dev/shm/ \
+--logs-dir logs/baseline_CC/50_pair \
+--height 256 --width 256
