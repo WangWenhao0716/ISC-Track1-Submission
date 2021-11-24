823b233da87e2008941770e5f01cd9538ac5d171
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-11-24 23:32:12 +0800

    Update dividing.sh

--- Test/augmentation/reference/dividing.sh
+++ Test/augmentation/reference/dividing.sh
@@ -1,20 +1,22 @@
-python dividing.py --num 0 &
-python dividing.py --num 1 &
-python dividing.py --num 2 &
-python dividing.py --num 3 &
-python dividing.py --num 4 &
-python dividing.py --num 5 &
-python dividing.py --num 6 &
-python dividing.py --num 7 &
-python dividing.py --num 8 &
-python dividing.py --num 9 &
-python dividing.py --num 10 &
-python dividing.py --num 11 &
-python dividing.py --num 12 &
-python dividing.py --num 13 &
-python dividing.py --num 14 &
-python dividing.py --num 15 &
-python dividing.py --num 16 &
-python dividing.py --num 17 &
-python dividing.py --num 18 &
-python dividing.py --num 19
+parallel -vk  ::: \
+'python dividing.py --num 0' &
+'python dividing.py --num 1' &
+'python dividing.py --num 2' &
+'python dividing.py --num 3' &
+'python dividing.py --num 4' &
+'python dividing.py --num 5' &
+'python dividing.py --num 6' &
+'python dividing.py --num 7' &
+'python dividing.py --num 8' &
+'python dividing.py --num 9' &
+'python dividing.py --num 10' &
+'python dividing.py --num 11' &
+'python dividing.py --num 12' &
+'python dividing.py --num 13' &
+'python dividing.py --num 14' &
+'python dividing.py --num 15' &
+'python dividing.py --num 16' &
+'python dividing.py --num 17' &
+'python dividing.py --num 18' &
+'python dividing.py --num 19'
+
