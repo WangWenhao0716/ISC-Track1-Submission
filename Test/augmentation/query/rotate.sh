12b25230e9e2b3a9413001344480f32f55b4127f
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-11-24 23:30:52 +0800

    Update rotate.sh

--- Test/augmentation/query/rotate.sh
+++ Test/augmentation/query/rotate.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-python rotate.py --num 0 &
-python rotate.py --num 1 &
-python rotate.py --num 2 &
-python rotate.py --num 3 &
-python rotate.py --num 4 &
-python rotate.py --num 5 &
-python rotate.py --num 6 &
-python rotate.py --num 7 &
-python rotate.py --num 8 &
-python rotate.py --num 9 &
-python rotate.py --num 10 &
-python rotate.py --num 11 &
-python rotate.py --num 12 &
-python rotate.py --num 13 &
-python rotate.py --num 14 &
-python rotate.py --num 15 &
-python rotate.py --num 16 &
-python rotate.py --num 17 &
-python rotate.py --num 18 &
-python rotate.py --num 19 
+parallel -vk  ::: \
+'python rotate.py --num 0' \
+'python rotate.py --num 1' \
+'python rotate.py --num 2' \
+'python rotate.py --num 3' \
+'python rotate.py --num 4' \
+'python rotate.py --num 5' \
+'python rotate.py --num 6' \
+'python rotate.py --num 7' \
+'python rotate.py --num 8' \
+'python rotate.py --num 9' \
+'python rotate.py --num 10' \
+'python rotate.py --num 11' \
+'python rotate.py --num 12' \
+'python rotate.py --num 13' \
+'python rotate.py --num 14' \
+'python rotate.py --num 15' \
+'python rotate.py --num 16' \
+'python rotate.py --num 17' \
+'python rotate.py --num 18' \
+'python rotate.py --num 19'
